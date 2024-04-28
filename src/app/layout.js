1f66d7e297d79ac3a4a5ecce5cbcf35a0aa1b752
FReptar0 <20213tn137@utez.edu.mx>
2024-04-28 15:18:52 -0600

    Implementando tema y ajustes de estilo

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,15 @@
 "use client";
-import theme from "@/utils/theme";
-import { ThemeProvider } from "@emotion/react";
+
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "@/utils/theme";
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AppRouterCacheProvider
-          options={{
-            cache: true,
-            cacheTTL: 1000 * 60 * 60 * 24,
-            cacheKey: "cacheKey",
-          }}
-        >
+        <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             {children}
           </ThemeProvider>
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,35 +1,16 @@
 import { createTheme } from '@mui/material/styles';
-import { Roboto } from 'next/font/google';
-
-const roboto = Roboto({
-    weights: [400, 500, 700],
-    display: 'swap',
-    subsets: ['latin-ext'],
-})
 
 const theme = createTheme({
-    typography: {
-        fontFamily: 'Roboto, sans-serif',
-    },
     palette: {
         mode: 'dark',
         primary: {
-            main: '#90caf9',
+            main: '#556cd6',
         },
         secondary: {
-            main: '#f48fb1',
+            main: '#19857b',
         },
-        error: {
-            main: '#f44336',
-        },
-        warning: {
-            main: '#ff9800',
-        },
-        info: {
-            main: '#2196f3',
-        },
-        success: {
-            main: '#4caf50',
+        background: {
+            default: '#fff',
         },
     },
 });
